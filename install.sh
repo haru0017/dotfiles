df91fef9c19c6c9abf9b4551f2dcd7ed898df4e1
haru0017 <hammerhead0017@gmail.com>
2021-10-30 15:05:53 +0900

    create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 未定義の変数があれば終了
+set -u
+
+# 現在のディレクトリを取得して移動
+THIS_DIR = $(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+echo "start setup..."
+for f in .??*; do
+  ["$f" = ".git"] && continue
+  ["$f" = ".gitconfig.local.template"] && continue
+
+  ln -snfv ${PWD}/"$f" ~/
+done
+
+cat << END
+
+**************************************************
+DOTFILES SETUP FINISHED! bye.
+**************************************************
+
+END
