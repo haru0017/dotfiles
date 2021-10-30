c61ec4541638ca0ae359a60fba7b353ed6bd0853
Tanaka Haruto <hammerhead0017@gmail.com>
2021-10-30 15:30:43 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 set -u
 
 # 現在のディレクトリを取得して移動
-THIS_DIR = $(cd $(dirname $0); pwd)
+THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
 echo "start setup..."
 for f in .??*; do
-  ["$f" = ".git"] && continue
-  ["$f" = ".gitconfig.local.template"] && continue
+  [ "$f" = ".git" ] && continue
+  [ "$f" = ".gitconfig.local.template" ] && continue
 
   ln -snfv ${PWD}/"$f" ~/
 done
