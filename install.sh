9e9cc2449e6cb127613d5b04c3e9e3a9804e4e31
haru0017 <hammerhead0017@gmail.com>
2022-09-11 11:36:41 +0900

    update all

--- install.sh
+++ install.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# 未定義の変数があれば終了
 set -u
 
-# 現在のディレクトリを取得して移動
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
+# dotfiles
 echo "start setup..."
 for f in .??*; do
   [ "$f" = ".git" ] && continue
-  [ "$f" = ".gitconfig.local.template" ] && continue
+  [ "$f" = ".gitignore" ] && continue
 
-  ln -snfv ${PWD}/"$f" ~/
+  ln -snfv ${THIS_DIR}/"$f" ~/
 done
+
+# other config
+ln -sf ${THIS_DIR}/fish/config.fish ~/.config/fish
+ln -sf ${THIS_DIR}/fish/fish_plugins ~/.config/fish
+ln -sf ${THIS_DIR}/karabiner.json ~/.config/karabiner/karabiner.json
+ 
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install packages and apps
+brew bundle
+
+# Set the default shell to fish
+echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+chsh -s /opt/homebrew/bin/fish
+
+# install fisher and packages
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+fish -c "fisher update"
 
 cat << END
 
