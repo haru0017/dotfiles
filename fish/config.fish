83724ee2e5a564adeb554efa9af37736f7bbc02b
haru0017 <hammerhead0017@gmail.com>
2022-09-11 11:40:27 +0900

    update .gitignore

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set fish_greeting ""
+
+# Homebrew
+eval (/opt/homebrew/bin/brew shellenv)
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# Key Bindings
+function fish_user_key_bindings
+  bind \cr peco_select_history # Bind for prco history to Ctrl+r
+end
