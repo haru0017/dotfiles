4cf9d7c264168cc50bfd0705f552d934b73dd613
haru0017 <hammerhead0017@gmail.com>
2023-02-26 15:08:29 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,9 @@ fish_add_path /opt/local/bin
 set -g GOPATH $HOME/go
 fish_add_path $GOPATH/bin
 
+# Rust
+fish_add_path $HOME/.cargo/bin
+
 # Key Bindings
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
@@ -17,3 +20,6 @@ end
 
 # nvm
 set -U nvm_default_version v16.17.1
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
