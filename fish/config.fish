cba5f5a46889c9d37c771bb5c33ad46e4022b496
haru0017 <hammerhead0017@gmail.com>
2023-04-12 10:09:19 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,13 @@ fish_add_path $GOPATH/bin
 # Rust
 fish_add_path $HOME/.cargo/bin
 
+# Python
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - | source
+
+fish_add_path $HOME/.local/bin
+
 # Key Bindings
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
