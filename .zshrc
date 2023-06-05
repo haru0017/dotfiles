6fd32e2cebdfca37da3d4d12962967efb75ec9f3
haru0017 <hammerhead0017@gmail.com>
2023-06-05 20:56:21 +0900

    u

--- .zshrc
+++ .zshrc
@@ -1,14 +1,13 @@
-# export NVM_DIR="$HOME/.nvm"
-#  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-#  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+# Go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
 
-# opam configuration
+# Python
+export PATH=$PATH:/opt/homebrew/bin/python3
+
+# OCaml
 [[ ! -r /Users/haru0017/.opam/opam-init/init.zsh ]] || source /Users/haru0017/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
 
-# alias
-
-## Python
+# Alias
 alias python='python3'
-
-## ocaml
 alias ocaml='rlwrap ocaml'
--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+
+# install packages and apps
+brew bundle
+
 set -u
 
 THIS_DIR=$(cd $(dirname $0); pwd)
@@ -9,32 +18,12 @@ cd $THIS_DIR
 echo "start setup..."
 for f in .??*; do
   [ "$f" = ".git" ] && continue
-  [ "$f" = ".gitignore" ] && continue
 
-  ln -snfv ${THIS_DIR}/"$f" ~/
+  ln -snfv ${THIS_DIR}/${f} ~/
 done
 
-# install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
-eval $(/opt/homebrew/bin/brew shellenv)
-
-# install packages and apps
-brew bundle
-
-# Set the default shell to fish
-echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
-chsh -s /opt/homebrew/bin/fish
-
 # other config
-ln -sf ${THIS_DIR}/fish/config.fish ~/.config/fish
-ln -sf ${THIS_DIR}/fish/fish_plugins ~/.config/fish
 ln -sf ${THIS_DIR}/karabiner.json ~/.config/karabiner/karabiner.json
-
-# install fisher and packages
-fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
-cd fish
-fish -c "fisher update"
 
 # terminal settings
 cd
