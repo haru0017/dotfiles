fe7590db92c4f12fadd1f15fe30a45e19ff0f13c
haru0017 <hammerhead0017@gmail.com>
2023-09-05 21:34:25 +0900

    add gsearch chrome extension

---
+++ chrome-extension/gsearch-shortcuts/dist/assets/content-script-loader.main.ts.565681f1.0d9cdd66.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  (async () => {
+    await import(
+      /* @vite-ignore */
+      chrome.runtime.getURL("assets/main.ts.565681f1.js")
+    );
+  })().catch(console.error);
+
+})();
